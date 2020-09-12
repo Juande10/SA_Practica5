14661dbca38e7531b434510c6c904638673df110
Juan de Dios Molina Herrera <juandem95@gmail.com>
2020-09-11 19:39:48 -0600

    Artefacto restaurante

---
+++ Restaurante/services/gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require("gulp");
+const zip = require("gulp-zip");
+
+gulp.task("zipper", function () {
+  return gulp.src("../*").pipe(zip("artifact.zip")).pipe(gulp.dest("../dist"));
+});
+
+gulp.task("default", gulp.series("zipper"));
